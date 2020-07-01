0254df08cbdd88e69ab1c715f7ee5b1672312576
Martti Malmi <sirius@iki.fi>
2020-07-01 15:11:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const webpush = require('web-push');
 
 const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
 const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+const contactEmail = process.env.CONTACT_EMAIL || 'mailto:user@example.com';
 
-// Replace with your email
-webpush.setVapidDetails('mailto:val@karpov.io', publicVapidKey, privateVapidKey);
+webpush.setVapidDetails(contactEmail, publicVapidKey, privateVapidKey);
 
 const app = express();
 
 app.use(require('body-parser').json());
 
 app.post('/subscribe', (req, res) => {
-  const subscription = req.body;
+  const subscription = req.body.subscription;
   res.status(201).json({});
-  const payload = JSON.stringify({ title: 'test' });
+  const payload = JSON.stringify(req.body.payload);
 
-  console.log(subscription);
+  console.log(req.body);
 
   webpush.sendNotification(subscription, payload).catch(error => {
     console.error(error.stack);
